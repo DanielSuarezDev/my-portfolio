c564a0cd9e7a8dc9071b1e4891c335f58eef3d4f
Daniel Suarez Dev <danielsuarez3114@gmail.com>
2024-03-29 22:28:39 -0500

    husky

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,23 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			h1: {
+				fontSize: '2.5rem',
+				fontWeight: '700',
+				lineHeight: '1.2',
+			},
+			h2: {
+				fontSize: '2rem',
+				fontWeight: '700',
+				lineHeight: '1.2',
+			},
+			h3: {
+				fontSize: '1.75rem',
+				fontWeight: '700',
+				lineHeight: '1.2',
+			},
+		},
 	},
 	plugins: [],
 }
