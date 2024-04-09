a7c3f73533763f99a70ed98be8f9520517b61fa8
Daniel Suarez Dev <danielsuarez3114@gmail.com>
2024-04-09 11:59:59 -0500

    import new

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@ import tailwind from "@astrojs/tailwind";
 
 import robotsTxt from "astro-robots-txt";
 
-// import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  // output: 'server',
-  // adapter: vercel({
-  //   webAnalytics: { enabled: true }
-  // }),
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   integrations: [tailwind(), robotsTxt()],
   site: 'https://porfolio.dev/',
 });
